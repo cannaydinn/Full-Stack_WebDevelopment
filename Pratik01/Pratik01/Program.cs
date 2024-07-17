248f73bea70b89086ae67a0e3ef9325939f3b50f
cannaydinn <canaydinn6@gmail.com>
2024-07-18 02:59:32 +0300

    #Konsol ekranına yazı yazma işlemi

---
+++ Pratik01/Pratik01/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+
+// Ad, Soyad ve Patika Bootcamp'inde olma nedenimi console ekranına yazdıran c# uygulamasını kodladım.
+Console.WriteLine("Can Aydın");
+Console.WriteLine("Bu bootcamp sayesinde bilgilerime bilgi katarak yetkinliklerimi arttırmayı planlıyorum.");
