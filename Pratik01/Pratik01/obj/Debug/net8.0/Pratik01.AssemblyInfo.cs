c2d281c8c4b986d7ecc3bc1f4ce648ca2b67b9cc
cannaydinn <canaydinn6@gmail.com>
2024-07-18 04:13:03 +0300

    Revert "Değişkenler ile Uygulama"
    This reverts commit 1a70c60e2bf7ea9930d0ddc6fbf9bb3cca78f83c.

--- Pratik01/Pratik01/obj/Debug/net8.0/Pratik01.AssemblyInfo.cs
+++ Pratik01/Pratik01/obj/Debug/net8.0/Pratik01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pratik01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248f73bea70b89086ae67a0e3ef9325939f3b50f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767402e8cf2e45f867387999e15f134de05048f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pratik01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pratik01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
