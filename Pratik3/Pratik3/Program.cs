d34ca4488a46ca7d0cf0f6d19e085be42c12f80b
Can Aydın <116949187+cannaydinn@users.noreply.github.com>
2024-07-18 15:40:26 +0300

    If / Else kullanımı

---
+++ Pratik3/Pratik3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+    konsol ekranı yardımıyla kullanıcıdan bir sayı değeri almanızı, 
+    bu değerin 10'a eşit mi , 10'dan küçük mü, 10'dan büyük mü olduğu durumları 
+    kontrol ederek kullanıcıya bir bilgilendirme mesajı yazmanız isteniyor.
+
+    Ardından 2. bir kontrol yapısıyla girilen sayının çift mi yoksa tek mi 
+    olduğunu ekrana yazdırınız.
+ 
+ */
+
+int sayi;
+
+Console.Write("Lütfen bir sayı giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+if(sayi == 10)
+{
+    Console.WriteLine("Girilen sayı 10'a eşittir.");
+}
+else if(sayi < 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+}
+else if(sayi > 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+}
+
+if(sayi % 2 == 0)
+{
+    Console.WriteLine("Girilen sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tektir.");
+}
