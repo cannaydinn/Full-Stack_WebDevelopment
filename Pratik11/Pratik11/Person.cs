f25425d41a1b71c8b83fb38bf595492e4a24a003
Can Aydın <116949187+cannaydinn@users.noreply.github.com>
2024-08-02 14:29:31 +0300

    Sınıf tanımlama ve nesne oluşturma

---
+++ Pratik11/Pratik11/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik11
+{
+    internal class Person
+    {
+
+        // özelliklerin tanımlanması
+        private string ad;
+        private string soyad;
+        private string dogumTarihi;
+
+
+        // özelliklerin public property'leri
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+
+        public string Soyad
+        {
+            get { return soyad; }
+            set { soyad = value; }  
+        }
+
+        public string DogumTarihi
+        {
+            get { return dogumTarihi; }
+            set { dogumTarihi = value; }
+        }
+
+    }
+}
---
+++ Pratik11/Pratik11/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Pratik11;
+
+// Person sınıfından ogrenci ve ogretmen nesneleri oluşturulması
+Person ogrenci =  new Person();
+Person ogretmen = new Person();
+
+// Person sınıfının özelliklerini kullanarak tanımlama işlemleri
+ogrenci.Ad = "Can";
+ogrenci.Soyad = "Aydın";
+ogrenci.DogumTarihi = "24 Temmuz";
+
+ogretmen.Ad = "Steve";
+ogretmen.Soyad = "Jobs";
+ogretmen.DogumTarihi = "24 Temmuz";
+
+
+// Konsol ekranına yazdırma işlemleri
+Console.WriteLine($"Öğrencinin Adı - Soyadı: {ogrenci.Ad} {ogrenci.Soyad}, Doğum Tarihi: {ogrenci.DogumTarihi}");
+
+Console.WriteLine($"Öğretmenin Adı - Soyadı: {ogretmen.Ad} {ogretmen.Soyad}, Doğum Tarihi: {ogretmen.DogumTarihi}");
