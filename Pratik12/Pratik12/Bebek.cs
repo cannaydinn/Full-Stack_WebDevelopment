5c572adf5a835c8ed36ce7993a1ab284c596888a
Can Aydın <116949187+cannaydinn@users.noreply.github.com>
2024-08-02 16:13:15 +0300

    Constructor Pratiği

---
+++ Pratik12/Pratik12/Bebek.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik12
+{
+    internal class Bebek
+    {
+        // Özelliklerin tanımlanması
+        private DateTime dogumTarihi;
+        private string ad;
+        private string soyad;
+
+        // Parametresiz Constructor
+        public Bebek()
+        {
+            dogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+       // Parametreli Constructor
+        public Bebek(string ad, string soyad)
+        {
+            this.ad = ad;
+            this.soyad = soyad;
+            dogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+
+        // Get/Set metotları
+        public DateTime DogumTarihi
+        {
+            get { return dogumTarihi; }
+            set { dogumTarihi = value; }
+        }
+
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+
+        public string Soyad
+        {
+            get { return soyad; }
+            set { soyad = value; }
+        }
+
+        
+
+        
+    }
+}
---
+++ Pratik12/Pratik12/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Pratik12;
+
+// Parametresiz Bebek nesnesi 
+Bebek bebek1 = new Bebek();
+
+bebek1.DogumTarihi = DateTime.Now;
+bebek1.Ad = "Can";
+bebek1.Soyad = "Aydın";
+
+Console.WriteLine($"Bebek 1 - Ad: {bebek1.Ad}, Soyad: {bebek1.Soyad}, Doğum Tarihi: {bebek1.DogumTarihi}");
+
+// Parametreli bebek nesnesi
+Bebek bebek2 = new Bebek("Can", "Aydın");
+
+Console.WriteLine($"Bebek 2 - Ad: {bebek2.Ad}, Soyad: {bebek2.Soyad}, Doğum Tarihi: {bebek2.DogumTarihi}");
+
+
+
+
+
+
+
+
+
