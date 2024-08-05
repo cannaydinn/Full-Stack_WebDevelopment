d98cc0fbddb8722b4414f0899b59395942114afc
Can Aydın <116949187+cannaydinn@users.noreply.github.com>
2024-08-05 17:38:19 +0300

    Constructor Yapısı

---
+++ Pratik13/Pratik13/Kitap.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik13
+{
+    internal class Kitap
+    {
+        private string kitapAdi;
+        private string yazarAd;
+        private string yazarSoyad;
+        private int sayfaSayisi;
+        private string yayinEvi;
+        private DateTime kayitTarihi;
+
+        
+        public Kitap()
+        {
+            kayitTarihi = DateTime.Now;
+        }
+
+        public Kitap(string kitapAdi, string yazarAd, string yazarSoyad, int sayfaSayisi, string yayinEvi)
+        {
+            this.kitapAdi = kitapAdi;
+            this.yazarAd = yazarAd;
+            this.yazarSoyad = yazarSoyad;
+            this.sayfaSayisi = sayfaSayisi;
+            this.yayinEvi = yayinEvi;
+            kayitTarihi = DateTime.Now;
+        }
+
+        public string KitapAdi
+        {
+            get{ return kitapAdi; }
+            set { kitapAdi = value; }
+        }
+
+        public string YazarAd
+        {
+            get { return yazarAd; }
+            set { yazarAd = value; }
+        }
+
+        public string YazarSoyad
+        {
+            get { return yazarSoyad; }
+            set { yazarSoyad = value; }
+        }
+
+        public int SayfaSayisi
+        {
+            get { return sayfaSayisi; }
+            set { sayfaSayisi = value; }
+        }
+
+        public string YayinEvi
+        {
+            get { return yayinEvi; }
+            set { yayinEvi = value; }
+        }
+
+        public DateTime KayitTarihi
+        {
+            get { return kayitTarihi; }
+            set { kayitTarihi = value; }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(yazarAd + " " + yazarSoyad + " adlı yazarın " + sayfaSayisi + " sayfalık " + yayinEvi + " tarafından çıkarılan " + kitapAdi + " adlı kitabı " +kayitTarihi +" tarihinde kütüphaneye eklendi");
+        }
+    }
+}
---
+++ Pratik13/Pratik13/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Pratik13;
+
+Kitap kitap = new Kitap();
+kitap.YazarAd = "Halide Edib";
+kitap.YazarSoyad = "Adıvar";
+kitap.KitapAdi = "Ateşten Gömlek";
+kitap.SayfaSayisi = 253;
+kitap.YayinEvi = "Can Yayınları";
+kitap.Show();
+
+Kitap kitap1 = new Kitap("Beyaz Zambaklar Ülkesinde", "Grigory", "Petrov", 140, "Koridor Yayıncılık");
+kitap1.Show();
+
+Kitap kitap2 = new Kitap("Notre Dame'ın Kamburu", "Victor", "Hugo", 656, "Can Yayınları");
+kitap2.Show();
+
+/*
+    Class: Kitap Sınıfı, nesneleri ve metotları tanımlamak için kullanılır.
+
+    Property: KitapAdi, YazarAdi, YazarSoyadi, SayfaSayisi, YayinEvi, ve KayitTarihi, Kitap sınıfının özellikleridir.
+
+    New: new anahtar kelimesi, yeni bir Kitap nesnesi oluşturmak için kullanılır.
+
+    Constructor: Kitap sınıfının iki constructor'ı vardır. Default constructor parametre almaz ve KayitTarihi'ni o anki tarih olarak atar. Alternatif constructor ise kitap adı, yazar adı, yazar soyadı, sayfa sayısı ve yayınevi bilgilerini alır ve bu bilgileri ilgili özelliklere atar.
+*/
+
+
+
+
