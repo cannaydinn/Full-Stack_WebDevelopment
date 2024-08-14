35265e0beaa57a0fbd5d38aaa7ccca102c47cb2e
Can Aydın <116949187+cannaydinn@users.noreply.github.com>
2024-08-14 15:06:23 +0300

    List (Liste) yapısı örnekleri

---
+++ Pratik20/Pratik20/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+// List yapısı kullaanılarak değerlerin ataması
+List<string> davetliler = new List<string>() { "Steve Jobs", "Bernard Arnault", "Elon Musk", "Jeff Bezos", "Mark Zuckerberg", "Can Aydın"};
+
+int sayac = 1;
+
+Console.WriteLine("*** Davetliler ***");
+
+// Liste elemanlarını yazdırma işlemi
+foreach (var item in davetliler)
+{
+    Console.WriteLine(sayac+ " - " +item);
+    sayac++;
+}
+
