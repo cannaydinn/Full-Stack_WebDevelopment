d0a78d745efb1996c4e1bb8d192cc909fe1f2245
Can Aydın <116949187+cannaydinn@users.noreply.github.com>
2024-08-14 15:48:35 +0300

    List(Liste) örnekleri 2

---
+++ Pratik21/Pratik21/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Kahve listesi oluşturma
+List<string> kahveListesi = new List<string>();
+
+
+// Konsoldan veri girişi ve listeye atama
+for(int i = 0; i < 5; i++)
+{
+    Console.Write($"Kahve {i+1}: ");
+    string kahve = Console.ReadLine();
+    kahveListesi.Add(kahve);
+}
+
+// Liste elemanlarını yazdırma
+Console.WriteLine("Girilen kahve isimleri: ");
+foreach(string str in kahveListesi)
+{
+    Console.WriteLine(str);
+}
