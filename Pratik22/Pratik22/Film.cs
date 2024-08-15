ef33149fa74a566d6c51bf34acde40ee3a659877
Can Aydın <116949187+cannaydinn@users.noreply.github.com>
2024-08-15 18:02:03 +0300

    List(Liste) kullanarak imdb örneği

---
+++ Pratik22/Pratik22/Film.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik22
+{
+    public class Film
+    {
+        // Sınıfın fiedları
+        private double imdbPuani;
+        private string filmAdi;
+
+        // Özelliklerin get/set metotları
+        public double ImdbPuani
+        {
+            get { return imdbPuani; }
+            set { imdbPuani = value; }
+        }
+
+        public string FilmAdi
+        {
+            get { return filmAdi; }
+            set { filmAdi = value; }
+        }
+
+
+        // Metot tanımlama
+        public void FilmGetir()
+        {
+            Console.WriteLine($"Film: {FilmAdi}, IMDb Puanı: {ImdbPuani}");
+        }
+
+
+        // İstenilen koşula göre metot tanımlama
+        public void IMDBPuani()
+        {
+            if( ImdbPuani >= 4 && ImdbPuani <=9)
+            {
+                Console.WriteLine($"Film: {FilmAdi}, IMDb Puanı: {ImdbPuani}");
+            }
+        }
+
+
+        // İstenilen koşula göre metot tanımlama
+        public void BaslangicHarfiA()
+        {
+            if (FilmAdi.ToLower().StartsWith("a"))
+                Console.WriteLine($"Film: {FilmAdi}, IMDb Puanı: {ImdbPuani}");
+
+        }
+    }
+}
---
+++ Pratik22/Pratik22/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using Pratik22;
+
+
+// Liste tanımlama
+// Liste tipi Film sınıfı
+List<Film> filmListesi = new List<Film>();
+string devam;
+
+do
+{
+    // Film Sınıfından nesne oluşturma
+    Film film = new Film();
+
+    Console.Write("\nFilm Adını giriniz: ");
+    film.FilmAdi = Console.ReadLine();
+
+    Console.Write("IMDB puanını giriniz: ");
+    film.ImdbPuani = Convert.ToDouble(Console.ReadLine());
+    
+    Console.Write("\nYeni bir film eklemek ister misiniz? (Evet/Hayır): ");
+    devam = Console.ReadLine().ToLower();
+    // Listeye elemanları ekleme
+    filmListesi.Add(film);
+
+}
+while (devam == "evet");
+
+
+Console.WriteLine("\n1 - Bütün filmleri listele");
+// Listedeki büütn elemanları Konsol ekranına yazar
+foreach (var film in filmListesi)
+{
+    film.FilmGetir();
+}
+
+// Koşula göre liste elemanlarını konsola yazar
+Console.WriteLine("\n2 - IMDB puanı 4 ile 9 arasında olan bütün filmleri listele");
+foreach (var film in filmListesi)
+{
+    film.IMDBPuani();
+}
+
+// Koşula göre liste elemanlarını konsola yazar
+Console.WriteLine("\n3 - İsmi 'A' ile başlayan filmler ve IMDB puanları listele");
+foreach(var film in filmListesi)
+{
+    film.BaslangicHarfiA();
+}
