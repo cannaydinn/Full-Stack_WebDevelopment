2eb1604472625aec66cca772ca6619c1f0762cc0
Can Aydın <116949187+cannaydinn@users.noreply.github.com>
2024-08-16 14:25:08 +0300

    Try-Catch Yapısı

---
+++ Pratik23/Pratik23/Program.cs
@@ -0,0 +1,18 @@
+﻿
+bool isValidInput = false;
+
+// Sayı girilinceye kadar çalışırs
+while (!isValidInput)
+{
+    // Hata Yakalama
+    try
+    {
+        Console.Write("Lütfen bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        isValidInput = true;
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+    }
+}
