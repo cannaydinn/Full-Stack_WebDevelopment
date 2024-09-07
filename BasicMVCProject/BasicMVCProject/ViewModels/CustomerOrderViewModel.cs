4026a68f22672a05316e0d998587c400b6862825
cannaydinn <canaydinn6@gmail.com>
2024-09-08 02:12:14 +0300

    Müşteri Sipariş Uygulaması

---
+++ BasicMVCProject/BasicMVCProject/ViewModels/CustomerOrderViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BasicMVCProject.Models;
+
+namespace BasicMVCProject.ViewModels
+{
+    public class CustomerOrderViewModel
+    {
+        private Customer customer;
+        private List<Order> orders;
+
+        public Customer Customer
+        {
+            get { return customer; }
+            set { customer = value; }
+        }
+
+        public List<Order> Orders
+        {
+            get { return orders; }
+            set { orders = value; }
+        }
+    }
+}
